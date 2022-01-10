8a1900c2ab7f4250de67a3026c4cdd5fc50533b6
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-01-11 00:54:20 +1030

    oxmysql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,6 +18,9 @@ client_scripts {
     'client/cl_bennys.lua'
 }
 
-server_script 'server/sv_bennys.lua'
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv_bennys.lua'
+}
 
 lua54 'yes'
